d19e32ece1b1536e97c6240e11c956e685e1c268
woniu336 <310478073@qq.com>
2023-10-11 14:29:01 +0800

    Docs: 『note』内容更新 2023-10-11 Wed 14:29:01

--- .shell/SSH_Proxy.sh
+++ .shell/SSH_Proxy.sh
@@ -1,9 +1,5 @@
-
 cat <<EOL >> ~/.ssh/config
-Host github.com
-    Hostname ssh.github.com
-    IdentityFile ~/.ssh/id_rsa
-    User git
-    Port 443
-    ProxyCommand "C:\Program Files\Git\mingw64\bin\connect.exe" -S 127.0.0.1:7890 %h %p
+Host github.com *.github.com  
+    User git  
+    ProxyCommand connect -S 127.0.0.1:7890 %h %p
 EOL
