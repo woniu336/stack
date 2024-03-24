d6b56812d4193b9e5e81b578c0413913e0614245
woniu336 <310478073@qq.com>
2024-03-25 05:28:35 +0800

    7522

--- .shell/cloudpull.bat
+++ .shell/cloudpull.bat
@@ -5,9 +5,10 @@ color 0A
 
 
 rem 执行rclone命令
-rclone copy oss:tvcdn\blog D:\blog --ignore-existing -u -v -P --transfers=20 --ignore-errors --buffer-size=128M --check-first --checkers=10 --drive-acknowledge-abuse --exclude "/{.git,public,resources}/" --exclude "*.mp4"
+rclone copy oss:tvcdn\blog D:\blog -u -v -P --transfers=20 --ignore-errors --buffer-size=128M --check-first --checkers=10 --drive-acknowledge-abuse --exclude "/{.git,public,resources}/" --exclude "*.mp4"
 
 rem 输出同步完毕消息
 echo.
-echo 同步完毕，按任意键退出...
+echo 拷贝完毕，按任意键退出...
 pause > nul
+exit
