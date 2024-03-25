8449d1a300e849fbf3a1d06a6617eb959f2065a0
woniu336 <310478073@qq.com>
2024-03-25 17:17:31 +0800

    提交更改: 2024-03-25 17:17:31

--- .shell/cloudpull.bat
+++ .shell/cloudpull.bat
@@ -5,7 +5,7 @@ color 0A
 
 
 rem 执行rclone命令
-rclone copy oss:tvcdn\blog D:\blog -u -v -P --transfers=20 --ignore-errors --buffer-size=128M --check-first --checkers=10 --drive-acknowledge-abuse --exclude "/{.git,public,resources}/" --exclude "*.mp4"
+rclone copy oss:tvcdn\blog D:\blog -u -v -P --transfers=20 --ignore-errors --buffer-size=128M --check-first --checkers=10 --drive-acknowledge-abuse --include "/{.obsidian,.shell}/**"
 
 rem 输出同步完毕消息
 echo.
--- .shell/cloudpush.bat
+++ .shell/cloudpush.bat
@@ -4,7 +4,7 @@ rem 设置提示消息的颜色为绿色高亮
 color 0A
 
 rem 执行rclone命令
-rclone sync D:\blog oss:tvcdn\blog -u -v -P --transfers=20 --ignore-errors --buffer-size=128M --check-first --checkers=10 --drive-acknowledge-abuse --exclude "/{.git,public,resources}/" --exclude "*.mp4"
+rclone sync D:\blog oss:tvcdn\blog -u -v -P --transfers=20 --ignore-errors --buffer-size=128M --check-first --checkers=10 --drive-acknowledge-abuse --include "/{.obsidian,.shell}/**"
 
 
 rem 输出同步完毕消息
